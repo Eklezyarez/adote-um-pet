3c25fb0b3b24c6b27738c8f3b5562e06a00d4a6a
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-12-09 21:49:52 -0300

    Update AdoteUmPetApplication.java

--- src/main/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplication.java
+++ src/main/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplication.java
@@ -1,4 +1,4 @@
-package br.com.treinaweb.adoteumpe
+package br.com.treinaweb.adoteump
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
