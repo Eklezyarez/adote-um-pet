b1006b865fba95d146473fa06a573016965185cb
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-12-10 23:45:47 -0300

    Update AdoteUmPetApplicationTests.java

--- src/test/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplicationTests.java
+++ src/test/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplicationTests.java
@@ -1,6 +1,6 @@
 package br.com.treinaweb.adoteumpet;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTe
+import org.springframework.boot.test.context.SpringBootT
 
 @SpringBootTest
 class AdoteUmPetApplicationTests {
